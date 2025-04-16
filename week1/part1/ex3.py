c646c35f5a7acc2ab257cfac184c398b6ddcaba0
Andrei Napruiu <napruiuandrei@gmail.com>
2025-04-16 11:25:57 +0300

    finished part 1

---
+++ week1/part1/ex3.py
@@ -0,0 +1,8 @@
+# Ask for an integer
+num = int(input("Enter an integer: "))
+
+# Check if the number is even or odd
+if num % 2 == 0:
+	print("The number is even.")
+else:
+	print("The number is odd.")
